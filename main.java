a3b391866d444221e00420d78b2853c18e03960f
BioFanat <bo.baoen@gmail.com>
2022-05-03 13:50:54 -0400

    added main

---
+++ main.java
@@ -0,0 +1,96 @@
+/**************************************************
+*   Author: Chi
+*   Date:  03 May 22
+**************************************************/
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.paint.Color;
+public class main extends Application
+{
+    enum Color{BLACK,WHITE};
+
+    public main()
+ 
+    {
+    }
+
+    @Override
+    public void init()
+    {
+    }
+
+    @Override
+    public void start(Stage primary)
+    {
+        
+        primary.show();
+    }
+
+    @Override
+    public void stop()
+    {
+    }
+
+    class Board {
+        Tile[][] tiles = new Tile[8][8];
+        Color current = Color.WHITE;
+        Color turn = Color.WHITE;
+
+        public Board(){ 
+            for (int i = 0; i < 8; i++){
+                for (int j = 0; j < 8; j++){
+                    tiles[i][j] = new Tile(current, i, j);
+                    if (current == Color.WHITE) {
+                        current = Color.BLACK;
+                    }
+                    else {
+                        current = Color.WHITE;
+                    }
+                }
+            }
+        }
+
+        public void clearGrey(){
+            for (int i = 0; i < 8; i++){
+                for (int j = 0; j < 8; j++) {
+                    if (tiles[i][j].isPossible){
+                        tiles[i][j].isPossible = false;
+                    }
+                }
+            }
+        }
+    }
+
+
+    class Tile extends Button{
+        
+        private Color color;
+        private boolean isPossible;
+        private int x, y;
+        //TODO: Add Contains Piece
+        
+        public Tile(Color color, int x, int y){
+            this.color = color;
+            this.x = x;
+            this.y=y;
+            setOnAction(e -> {
+                if (isPossible){
+                    
+                }
+
+               //TODO: call current piece's show moves 
+            });
+            System.out.println("-fx-background-color:" + color.toString().toLowerCase());
+            setStyle("-fx-background-color:" + color.toString().toLowerCase());
+        }
+        
+    
+
+        
+
+    }
+
+}
