a4b9c71c64a02d6ed5c0791911ab18abc1712ed4
BioFanat <bo.baoen@gmail.com>
2022-05-04 15:17:48 -0400

    commit

--- main.java
+++ main.java
@@ -2,19 +2,24 @@
 *   Author: Chi
 *   Date:  03 May 22
 **************************************************/
+import java.util.Optional;
+
 import javafx.application.Application;
 import javafx.application.Platform;
+import javafx.beans.property.SimpleObjectProperty;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
 import javafx.scene.paint.Color;
 public class main extends Application
 {
     enum Color{BLACK,WHITE};
 
     public main()
- 
     {
+        
     }
 
     @Override
@@ -26,20 +31,30 @@ public class main extends Application
     public void start(Stage primary)
     {
         
+        BorderPane bp = new BorderPane();
+        GridPane gamePiece = new GridPane();
+        Board game = new Board(gamePiece);
+        bp.setCenter(gamePiece);
+        Scene main = new Scene(bp, 800, 500);
+        primary.setScene(main);
         primary.show();
     }
 
     @Override
     public void stop()
     {
+
     }
 
     class Board {
         Tile[][] tiles = new Tile[8][8];
-        Color current = Color.WHITE;
+        Color start = Color.WHITE;
+        Color current = start;
         Color turn = Color.WHITE;
+        GridPane display;
 
-        public Board(){ 
+        public Board(GridPane display){
+            this.display = display; 
             for (int i = 0; i < 8; i++){
                 for (int j = 0; j < 8; j++){
                     tiles[i][j] = new Tile(current, i, j);
@@ -49,6 +64,14 @@ public class main extends Application
                     else {
                         current = Color.WHITE;
                     }
+                    display.add(tiles[i][j], i, j);
+
+                }
+                if (current == Color.WHITE) {
+                    current = Color.BLACK;
+                }
+                else {
+                    current = Color.WHITE;
                 }
             }
         }
@@ -70,12 +93,14 @@ public class main extends Application
         private Color color;
         private boolean isPossible;
         private int x, y;
+        Optional<SimpleObjectProperty<Piece>> currentPiece;
         //TODO: Add Contains Piece
         
         public Tile(Color color, int x, int y){
             this.color = color;
             this.x = x;
             this.y=y;
+            currentPiece = Optional.empty();
             setOnAction(e -> {
                 if (isPossible){
                     
@@ -86,11 +111,15 @@ public class main extends Application
             System.out.println("-fx-background-color:" + color.toString().toLowerCase());
             setStyle("-fx-background-color:" + color.toString().toLowerCase());
         }
-        
-    
 
-        
+        public Optional<SimpleObjectProperty<Piece>> getCurrentPiece(){
+            return currentPiece;
+        }
 
+        public void setCurrentPiece(SimpleObjectProperty<Piece> piece){
+            currentPiece = Optional.of(piece);
+        }
+        
     }
 
 }
