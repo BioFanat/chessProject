071250a977c05338db0ea33d225a6870199bce65
Imperite <dsbradley03@gmail.com>
2022-05-04 15:23:21 -0400

    Refactored to allow easier use on other computers

--- main.java
+++ main.java
@@ -13,12 +13,11 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
-public class main extends Application
-{
 
-    public main()
-    {
-        
+public class Main extends Application {
+
+    public Main() {
+
     }
 
     @Override
@@ -26,8 +25,7 @@ public class main extends Application
     }
 
     @Override
-    public void start(Stage primary)
-    {
+    public void start(Stage primary) {
         BorderPane bp = new BorderPane();
         GridPane gamePiece = new GridPane();
         Board game = new Board(gamePiece);
@@ -40,6 +38,10 @@ public class main extends Application
     @Override
     public void stop() {
     }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
 
 class Board {
@@ -50,15 +52,14 @@ class Board {
 
     GridPane display;
 
-    public Board(GridPane display){
-        this.display = display; 
-        for (int i = 0; i < 8; i++){
-            for (int j = 0; j < 8; j++){
+    public Board(GridPane display) {
+        this.display = display;
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
                 tiles[i][j] = new Tile(current, i, j);
                 if (current == Color.WHITE) {
                     current = Color.BLACK;
-                }
-                else {
+                } else {
                     current = Color.WHITE;
                 }
                 display.add(tiles[i][j], i, j);
@@ -66,13 +67,11 @@ class Board {
             }
             if (current == Color.WHITE) {
                 current = Color.BLACK;
-            }
-            else {
+            } else {
                 current = Color.WHITE;
             }
         }
     }
-    
 
     public void clearGrey() {
         for (int i = 0; i < 8; i++) {
@@ -89,58 +88,56 @@ enum Color {
     BLACK, WHITE
 };
 
-    class Tile extends Button{
-        
-        private Color color;
-        private boolean isPossible;
-        private int x, y;
-        Optional<SimpleObjectProperty<Piece>> currentPiece;
-        //TODO: Add Contains Piece
-        
-        public Tile(Color color, int x, int y){
-            this.color = color;
-            this.x = x;
-            this.y=y;
-            currentPiece = Optional.empty();
-            setOnAction(e -> {
-                if (isPossible){
-                    
-                }
+class Tile extends Button {
 
-               //TODO: call current piece's show moves 
-            });
-            System.out.println("-fx-background-color:" + color.toString().toLowerCase());
-            setStyle("-fx-background-color:" + color.toString().toLowerCase());
-        }
+    private Color color;
+    private boolean isPossible;
+    private int x, y;
+    Optional<SimpleObjectProperty<Piece>> currentPiece;
+    // TODO: Add Contains Piece
 
-        public Optional<SimpleObjectProperty<Piece>> getCurrentPiece(){
-            return currentPiece;
-        }
+    public Tile(Color color, int x, int y) {
+        this.color = color;
+        this.x = x;
+        this.y = y;
+        currentPiece = Optional.empty();
+        setOnAction(e -> {
+            if (isPossible) {
 
-        public void setCurrentPiece(SimpleObjectProperty<Piece> piece){
-            currentPiece = Optional.of(piece);
-        }
+            }
 
-        public Color getColor() {
-            return color;
-        }
-    
-        public boolean isPossible() {
-            return isPossible;
-        }
-    
-        public void setPossible(boolean isPossible) {
-            this.isPossible = isPossible;
-        }
-    
-        public int getX() {
-            return x;
-        }
-    
-        public int getY() {
-            return y;
-        }
-        
+            // TODO: call current piece's show moves
+        });
+        System.out.println("-fx-background-color:" + color.toString().toLowerCase());
+        setStyle("-fx-background-color:" + color.toString().toLowerCase());
     }
 
+    public Optional<SimpleObjectProperty<Piece>> getCurrentPiece() {
+        return currentPiece;
+    }
 
+    public void setCurrentPiece(SimpleObjectProperty<Piece> piece) {
+        currentPiece = Optional.of(piece);
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public boolean isPossible() {
+        return isPossible;
+    }
+
+    public void setPossible(boolean isPossible) {
+        this.isPossible = isPossible;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+}
